b4f7dd8abc0e6e6f66edc1ecb49103f402ed42e5
Doug A. Letwin <dletwin7506@gmail.com>
2014-09-12 14:09:11 -0400

    changes file names in images

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Fcjc::Application.routes.draw do
   resources :newsletters, except: [:edit, :update]
 
   get "static_pages/index"
+  get "static_pages/about"
 
   # If you list the "as" portion of the routes, you can refer
   # to them by name in your code. ie link_to "About"
   # You will still need a StaticPagesController with actions defined
   # for all these pages...
-
-  get "/about", "static_pages#about" as: "about"
+  # get "/about", "static_pages#about" as: "about"
 
   get "static_pages/mission"
   get "static_pages/contact_us"
