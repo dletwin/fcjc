5c48a810bd0a8021f61234fd50dd4f43d4ebe5d2
Doug Letwin <dletwin7506@gmail.com>
2014-09-12 22:24:11 -0400

    went through and made changes according to suggestions from Matt Darby

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,21 @@
 Fcjc::Application.routes.draw do
   resources :newsletters, except: [:edit, :update]
 
-  get "static_pages/index"
+  root "static_pages#index"
+  get "/index" => "static_pages#index", as: "index"
   get "/about" => "static_pages#about", as: "about"
   get "/mission" => "static_pages#mission", as: "mission"
-  # If you list the "as" portion of the routes, you can refer
-  # to them by name in your code. ie link_to "About"
-  # You will still need a StaticPagesController with actions defined
-  # for all these pages...
-   #get "/about", "static_pages#about" as: "about"
-
-
-  get "static_pages/contact_us"
-  get "static_pages/gallery"
-  get "static_pages/links"
-  get "static_pages/prayers"
-  get "static_pages/youth"
-  get "static_pages/fcjc_pray"
-  get "static_pages/cp_eng"
-  get "static_pages/cp_lat"
-  get "static_pages/sacrament"
-  get "static_pages/pledge"
-  get "static_pages/calender"
-  get "static_pages/prayer_gathering"
-
-  root "static_pages#index"
+  get "/contact_us" => "static_pages#contact_us", as: "contact_us"
+  get "/gallery" => "static_pages#gallery", as: "gallery"
+  get "/links" => "static_pages#links", as: "links"
+  get "/prayers" => "static_pages#prayers", as: "prayers"
+  get "/youth" => "static_pages#youth", as: "youth"
+  get "/fcjc_pray" => "static_pages#fcjc_pray", as: "fcjc_pray"
+  get "/cp_eng" => "static_pages#cp_eng", as: "cp_eng"
+  get "/cp_lat" => "static_pages#cp_lat", as: "cp_lat"
+  get "/sacrament" => "static_pages#sacrament", as: "sacrament"
+  get "/pledge" => "static_pages#pledge", as: "pledge"
+  get "/calender" => "static_pages#calender", as: "calender"
+  get "/prayer_gathering" => "static_pages#prayer_gathering", as: "prayer_gathering"
 
 end
