94697b32364509b58b10087e75ddca90f5d27fe3
Doug Letwin <dletwin7506@gmail.com>
2014-09-12 21:32:03 -0400

    working on recoding routes using as:

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Fcjc::Application.routes.draw do
   resources :newsletters, except: [:edit, :update]
 
   get "static_pages/index"
-  get "static_pages/about"
-
+  get "/about" => "static_pages#about", as: "about"
+  get "/mission" => "static_pages#mission", as: "mission"
   # If you list the "as" portion of the routes, you can refer
   # to them by name in your code. ie link_to "About"
   # You will still need a StaticPagesController with actions defined
   # for all these pages...
-  # get "/about", "static_pages#about" as: "about"
+   #get "/about", "static_pages#about" as: "about"
 
-  get "static_pages/mission"
+
   get "static_pages/contact_us"
   get "static_pages/gallery"
   get "static_pages/links"
@@ -25,4 +25,5 @@ Fcjc::Application.routes.draw do
   get "static_pages/prayer_gathering"
 
   root "static_pages#index"
+
 end
