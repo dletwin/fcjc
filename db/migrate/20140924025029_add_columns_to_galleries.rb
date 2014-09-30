48e56fda0ee655f1ab8dd793071673b5f0050eae
Doug A. Letwin <dletwin7506@gmail.com>
2014-09-30 16:00:56 -0400

    corrected the select boxes for the image galleries. Adjusted style sheets

--- db/migrate/20140924025029_add_columns_to_galleries.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToGalleries < ActiveRecord::Migration
-  def change
-    add_column :galleries, :month, :string
-    add_column :galleries, :day, :integer
-    add_column :galleries, :year, :integer
-  end
-end
