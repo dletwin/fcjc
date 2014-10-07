ab2dc361f6785f3754e95dcc0efeaddb87ebed4c
Doug A. Letwin <dletwin7506@gmail.com>
2014-10-07 14:08:10 -0400

    made minor changes

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -2,5 +2,4 @@ class Newsletter < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader # Uploader used for this model
 
   validates :title, :month, :year, presence: true
-  validates :year, length: { is: 4 } # Make sure year is present and is 4 integers long
 end
